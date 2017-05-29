81e2a82961f99df0b03868ee559c8c9550c4a72a
John Sauter <John_Sauter@systemeyescomputerstore.com>
2017-05-29 01:44:42 -0400

    add an example project, and prepare for a second

---
+++ sample/run_sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# File: run_sample.sh, author: John Sauter, date: May 28, 2017
+export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
+sound_effects_player  Sample_project.xml
+
+# End of file run_sample.sh
+
--- sample/try_player.sh
+++
@@ -1,3 +0,0 @@
-export G_SLICE=debug-blocks
-export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
-sound_effects_player  Sample_project.xml
